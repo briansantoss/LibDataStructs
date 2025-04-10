0315710ce53e8b21540be92abdcc0d0d5a416a6f
142011551+briansantoss@users.noreply.github.com <142011551+briansantoss@users.noreply.github.com>
2025-04-10 01:31:49 -0300

    chore: simplificando amostra

--- sample/sample.c
+++ sample/sample.c
@@ -6,23 +6,21 @@ int main() {
     if (list == NULL) {
         perror("Error: cannot create the list.");
     }
-
     
     list_push_int(list, 10);
-    list_push_int(list, 35);
-    
-    list_push_bool(list, false);
     
     list_push_end_bool(list, true);
-
-    list_display(list);
     
     list_insert_char_at(list, 1, 'e');
-
-    list_insert_string_at(list, 4, "Erika");
+    
+    list_remove_at(list, 2);
+    
+    list_insert_string_at(list, 2, "Erika");
     
     list_display(list);
 
+    printf("Linked list len: %lld\n", list_len(list));
+    
     list_free(list);
 
     list_display(list);
